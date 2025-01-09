c1e5e0cab806dffb3751edaf675b40b04722182d
samuka7abr <samuelabrao2006@gmail.com>
2025-01-09 14:30:37 -0300

    request response

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();//estrutura básica de uma aplicação utilizando express
 
-app.get("/", (request, response) => {
-    response.send("Hello, World!")
+app.get("/message/:id/:user", (request, response) => {
+    const { user, id } = request.param
+
+    response.send(`ID da mensagem: ${id}
+        user: ${user}`)
+    
 });
 
 const PORT = 3333;
-//estrutura básica de uma aplicação utilizando express
 app.listen(PORT, () => console.log(`server is running on PORT ${PORT}`));
