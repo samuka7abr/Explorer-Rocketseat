66037a1b14129f335927d8039a4a03183b5104a1
samuka7abr <samuelabrao2006@gmail.com>
2025-01-09 19:18:09 -0300

    create function

---
+++ src/controllers/UsersController.js
@@ -0,0 +1,7 @@
+class UsersController {
+    create(request, response){
+    const { name, email, password } = request.body
+    response.json({ name, email, password })
+    }
+}
+module.exports = UsersController;
