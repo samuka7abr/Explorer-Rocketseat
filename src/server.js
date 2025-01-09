1c7bb8093dc44730a05ffb4a8fc4532ba08aa85f
samuka7abr <samuelabrao2006@gmail.com>
2025-01-09 18:13:08 -0300

    Nodemon

--- src/server.js
+++ src/server.js
@@ -2,12 +2,18 @@ const express = require("express");
 const app = express();//estrutura básica de uma aplicação utilizando express
 
 app.get("/message/:id/:user", (request, response) => {
-    const { user, id } = request.param
+    const { user, id } = request.params
 
     response.send(`ID da mensagem: ${id}
         user: ${user}`)
     
 });
 
+app.get("/users", (request, response) => {
+    const { page, limit} = request.query
+    response.send(`Página: ${page} Mostrar: ${limit}`)
+
+
+})
 const PORT = 3333;
 app.listen(PORT, () => console.log(`server is running on PORT ${PORT}`));
