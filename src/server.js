43da612b0085cd04ec053ad0c43773101f801760
samuka7abr <samuelabrao2006@gmail.com>
2025-01-09 18:53:56 -0300

    routes module

--- src/server.js
+++ src/server.js
@@ -1,19 +1,10 @@
-const express = require("express");
-const app = express();//estrutura básica de uma aplicação utilizando express
+const express = require('express')
+const routes = require("./routes")
 
-app.get("/message/:id/:user", (request, response) => {
-    const { user, id } = request.params
+const app = express()
+app.use(express.json())
 
-    response.send(`ID da mensagem: ${id}
-        user: ${user}`)
-    
-});
+app.use(routes)
 
-app.get("/users", (request, response) => {
-    const { page, limit} = request.query
-    response.send(`Página: ${page} Mostrar: ${limit}`)
-
-
-})
-const PORT = 3333;
-app.listen(PORT, () => console.log(`server is running on PORT ${PORT}`));
+const PORT = 3333
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
