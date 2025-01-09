52aeb1c15b3a79acd1efffb8c3ff979ac48f9381
samuka7abr <samuelabrao2006@gmail.com>
2025-01-09 10:53:25 -0300

    GET method implementation

--- src/server.js
+++ src/server.js
@@ -1,5 +1,10 @@
 const express = require("express");
-const app = express();
+const app = express();//estrutura básica de uma aplicação utilizando express
+
+app.get("/", (request, response) => {
+    response.send("Hello, World!")
+});
+
 const PORT = 3333;
 //estrutura básica de uma aplicação utilizando express
 app.listen(PORT, () => console.log(`server is running on PORT ${PORT}`));
