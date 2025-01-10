3d7fb142ba89f2bff5d532d868954856646f4de7
samuka7abr <samuelabrao2006@gmail.com>
2025-01-10 20:29:49 -0300

    Update users.routes.js

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -2,8 +2,14 @@ const { Router } = require("express")
 const UsersController = require("../controllers/UsersController");
 
 const usersRoutes = Router()
+
+function myMiddleware(request, response, next){ 
+    console.log('vc pssou pelo middleware')
+}
+
+
 const usersController = new UsersController();
 
-usersRoutes.post('/', usersController.create)
+usersRoutes.post('/', myMiddleware, usersController.create)
 
 module.exports = usersRoutes;
