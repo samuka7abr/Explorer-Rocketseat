38680a58841142d7ae066cd21c1f4bc06fc0d9a4
samuka7abr <samuelabrao2006@gmail.com>
2025-01-10 20:48:36 -0300

    middleware

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,12 +1,8 @@
 const { Router } = require("express")
+
 const UsersController = require("../controllers/UsersController");
 
 const usersRoutes = Router()
-
-function myMiddleware(request, response, next){ 
-    console.log('vc pssou pelo middleware')
-}
-
 
 const usersController = new UsersController();
 
