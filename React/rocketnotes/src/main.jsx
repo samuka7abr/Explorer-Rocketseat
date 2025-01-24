1d26653481b7848caad2c71be0a8ae3e18b10516
samuka7abr <samuelabrao2006@gmail.com>
2025-01-24 15:32:05 -0300

    chore: global themes implemented

--- React/rocketnotes/src/main.jsx
+++ React/rocketnotes/src/main.jsx
@@ -1,9 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { Details } from './pages/Details/index.jsx'
+import React from "react"
+import ReactDOM from 'react-dom/client'
+import { ThemeProvider } from "styled-components"
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Details />
-  </StrictMode>,
+import theme from './styles/theme'
+
+import { Details } from "./pages/Details"
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ThemeProvider theme = {theme}>
+      <Details/>
+    </ThemeProvider>
+  </React.StrictMode>
 )
