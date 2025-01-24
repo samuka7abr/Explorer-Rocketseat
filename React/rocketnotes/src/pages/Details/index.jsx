fce3819aec9de7d32f9d79410dbe708bfd2604c0
samuka7abr <samuelabrao2006@gmail.com>
2025-01-24 15:25:06 -0300

    chore: styled components added to project

---
+++ React/rocketnotes/src/pages/Details/index.jsx
@@ -0,0 +1,10 @@
+import { Container } from "./styles"
+
+export function Details(){
+  return(
+    <Container>
+    <h1>hello world</h1>
+    <p>Samuel Abrão</p>
+    </Container>
+  )
+}
---
+++ React/rocketnotes/src/pages/Details/styles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    background: green;
+    width: 200px;
+
+`
