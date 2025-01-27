5856ca3c88e272d2b2351ba6de5336f6b97762bb
samuka7abr <samuelabrao2006@gmail.com>
2025-01-27 11:16:42 -0300

    chore: util links component

--- React/rocketnotes/src/pages/Details/index.jsx
+++ React/rocketnotes/src/pages/Details/index.jsx
@@ -2,11 +2,24 @@ import { Container } from "./styles";
 
 import { Header } from '../../components/Header';
 import { Button } from '../../components/Button';
+import { Section } from "../../components/Section";
+import { Links } from "../../components/Section/styles";
 
 export function Details(){
   return(
     <Container>
       <Header />
+
+      <Section title= "Links úteis">
+
+        <Links>
+          <li> <a href="#">http://github/samuka7abr</a></li>
+          <li> <a href="#">http://github/samuka7abr</a></li>
+        </Links>
+
+      </Section>
+
+
     <Button title="voltar"/>
     </Container>
     
