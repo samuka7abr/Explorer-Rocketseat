ad5161cdcefe6f97b40a655a149fb89afe6ca9b8
samuka7abr <samuelabrao2006@gmail.com>
2025-01-27 20:19:46 -0300

    feat: logo style and position set

--- React/rocketnotes/src/main.jsx
+++ React/rocketnotes/src/main.jsx
@@ -5,6 +5,7 @@ import { ThemeProvider } from "styled-components"
 import GlobalStyles from './styles/global'
 import theme from './styles/theme'
 
+import { Home } from "./pages/Home"
 import { Details } from "./pages/Details"
 
 
@@ -12,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme = {theme}>
       <GlobalStyles/>
-      <Details/>
+      <Home/>
     </ThemeProvider>
   </React.StrictMode>
 )
