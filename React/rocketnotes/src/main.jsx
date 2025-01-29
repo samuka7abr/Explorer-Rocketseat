e5ca5974b1427dcd57a7d6c7a9ab872f6764c30b
samuka7abr <samuelabrao2006@gmail.com>
2025-01-29 16:25:50 -0300

    chore: user page forms

--- React/rocketnotes/src/main.jsx
+++ React/rocketnotes/src/main.jsx
@@ -5,6 +5,7 @@ import { ThemeProvider } from "styled-components"
 import GlobalStyles from './styles/global'
 import theme from './styles/theme'
 
+import { Profile } from "./pages/Profile"
 import { SignIn } from "./pages/SignIn"
 import { SignUp } from "./pages/SignUp"
 import { Home } from "./pages/Home"
@@ -15,7 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme = {theme}>
       <GlobalStyles/>
-      <SignUp/>
+      <Profile/>
     </ThemeProvider>
   </React.StrictMode>
 )
