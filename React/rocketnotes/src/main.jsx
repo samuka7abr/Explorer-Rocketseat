cd66089a7362b9cfb50dee75f7fa3e1300dd990d
samuka7abr <samuelabrao2006@gmail.com>
2025-01-29 11:16:18 -0300

    feat: signUp page

--- React/rocketnotes/src/main.jsx
+++ React/rocketnotes/src/main.jsx
@@ -6,6 +6,7 @@ import GlobalStyles from './styles/global'
 import theme from './styles/theme'
 
 import { SignIn } from "./pages/SignIn"
+import { SignUp } from "./pages/SignUp"
 import { Home } from "./pages/Home"
 import { Details } from "./pages/Details"
 
@@ -14,7 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme = {theme}>
       <GlobalStyles/>
-      <SignIn/>
+      <SignUp/>
     </ThemeProvider>
   </React.StrictMode>
 )
