2e27d67326f2b1c92ec374c2e9169ad908ff26e5
samuka7abr <samuelabrao2006@gmail.com>
2025-02-01 12:45:42 -0300

    feat: New Note page

--- React/rocketnotes/src/components/NoteItem/index.jsx
+++ React/rocketnotes/src/components/NoteItem/index.jsx
@@ -1,23 +1,24 @@
-import { FiPlus, FiX } from "react-icons/fi";
-import { Container } from "./styles";
+import { FiPlus, FiX } from 'react-icons/fi'
 
-export function NoteItem({ isNew, value, onClick, ...rest}){
-    return(
-        <Container isNew={isNew}>
-            <input 
-                type="text"
-                value={value}
-                readOnly={!isNew}
-                {...rest}
-            />
-            <button 
-                type="button"
-                onClick={onClick}
-            >
-                {isNew ? <FiPlus /> : <FiX />}
-            </button>
-        </Container>
-)
+import { Container } from './styles'
 
+export function NoteItem({ isNew, value, onClick, ...rest }) {
+  return (
+    <Container isNew={isNew}>
+      <input
+        type="text"
+        value={value}
+        readOnly={!isNew}
+        {...rest}
+      />
 
+      <button
+        type="button"
+        onClick={onClick}
+        className={isNew ? 'button-add' : 'button-delete'}
+      >
+        {isNew ? <FiPlus /> : <FiX />}
+      </button>
+    </Container>
+  )
 }
--- React/rocketnotes/src/pages/New/index.jsx
+++ React/rocketnotes/src/pages/New/index.jsx
@@ -1,6 +1,9 @@
 import { Header } from '../../components/Header'
 import { Input } from '../../components/Input'
 import { Textarea } from '../../components/Textarea'
+import { NoteItem} from '../../components/NoteItem'
+import { Section } from '../../components/Section'
+import { Button } from '../../components/Button'
 
 import { Container, Form } from './styles'
 
@@ -18,6 +21,21 @@ export function New() {
 
           <Input placeholder="Título" />
           <Textarea placeholder="Observações" />
+
+          <Section title="Links úteis">
+            <NoteItem value="https://rocketseat.com.br"/>
+            <NoteItem isNew placeholder="Novo Link"/>
+
+          </Section>
+
+          <div className="tags">
+            <Section title="Marcadores">
+              <NoteItem value="React"/>
+              <NoteItem isNew placeholder="Nova tag"/>
+            </Section>
+          </div>
+
+          <Button title="Salvar"/> 
         </Form>
       </main>
     </Container>
