c7cd27ec06c6513eec45361bf32e7ebbcdd006f9
samuka7abr <samuelabrao2006@gmail.com>
2025-02-01 14:37:05 -0300

    feat: AppRoutes and AuthRoutes

--- React/rocketnotes/src/main.jsx
+++ React/rocketnotes/src/main.jsx
@@ -5,19 +5,14 @@ import { ThemeProvider } from "styled-components"
 import GlobalStyles from './styles/global'
 import theme from './styles/theme'
 
-import { Profile } from "./pages/Profile"
-import { SignIn } from "./pages/SignIn"
-import { SignUp } from "./pages/SignUp"
-import { Home } from "./pages/Home"
-import { Details } from "./pages/Details"
-import { New } from './pages/New'
+import { Routes } from './routes'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme = {theme}>
       <GlobalStyles/>
-      <New />
+      <Routes />
     </ThemeProvider>
   </React.StrictMode>
 )
--- React/rocketnotes/src/pages/SignIn/index.jsx
+++ React/rocketnotes/src/pages/SignIn/index.jsx
@@ -1,6 +1,8 @@
 import { FiMail, FiLock } from 'react-icons/fi'
 import { Background } from './styles'
 
+import { Link } from 'react-router-dom';
+
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
 
