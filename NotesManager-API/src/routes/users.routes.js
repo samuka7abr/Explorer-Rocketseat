059ff26a4891427446351f2398b6f24993dbb1ac
samuka7abr <samuelabrao2006@gmail.com>
2025-02-10 12:23:00 -0300

    feat: saving userAvatar picture in DB

--- NotesManager-API/src/routes/users.routes.js
+++ NotesManager-API/src/routes/users.routes.js
@@ -3,17 +3,16 @@ const { Router } = require('express');
 const multer = require('multer');
 
 const UsersController = require('../controllers/UsersController');
+const UserAvatarController = require('../controllers/UserAvatarController');
 const ensureAuthenticated = require('../middlewares/ensureAuthenticated');
 
 const usersRoutes = Router();
 const upload = multer(uploadConfig.MULTER);
 
 const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
 
 usersRoutes.post('/', usersController.create);
 usersRoutes.put('/', ensureAuthenticated, usersController.update);
-usersRoutes.patch('/avatar', ensureAuthenticated, upload.single("avatar"), (request, response) =>{
-    console.log(request.file.filename);
-    response.json();
-} );
+usersRoutes.patch('/avatar', ensureAuthenticated, upload.single("avatar"), userAvatarController.update);
 module.exports = usersRoutes;
